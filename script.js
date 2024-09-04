d3e6770325bf2bd76e5ee37102bb9e74bebdae0b
Praveen Singh <singh.praveen1431@outlook.com>
2024-09-04 10:25:14 +0530

    RPS ready to play

---
+++ script.js
@@ -0,0 +1,42 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const display = document.getElementById("display");
+const result = display.querySelector("p");
+
+let player = null;
+let computer = null;
+
+function computerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function playerChoice(choice) {
+  player = choice;
+  console.log("Player choice:", player);
+  computer = computerChoice();
+  console.log("Computer choice:", computer);
+  game();
+}
+
+rock.addEventListener("click", () => playerChoice("rock"));
+paper.addEventListener("click", () => playerChoice("paper"));
+scissors.addEventListener("click", () => playerChoice("scissors"));
+
+function game() {
+  if (player === computer) {
+    return (result.textContent = "Draw!");
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    return (result.textContent = `You Win! Computer picked: ${computer}`);
+  } else {
+    return (result.textContent = `You Lose! Computer picked: ${computer}`);
+  }
+}
+
+
